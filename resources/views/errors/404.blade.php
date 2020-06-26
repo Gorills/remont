3ea127efc510c89d9be5469f02e08eeda211c505
Gorills <gorivanickiy@gmail.com>
2020-06-26 22:17:17 +0700

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,15 +12,7 @@
 @section('twitter:title', '')
 
 @section('content')
-    <div class="container">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ url('/') }}">Главная</a></li>
 
-                <li class="breadcrumb-item active" aria-current="page">404 ошибка</li>
-            </ol>
-        </nav>
-    </div>
 
 
     <div class="error">
