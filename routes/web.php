963c97908780938cf3d880abcd9c41ee9d7e25ef
Gorills <gorivanickiy@gmail.com>
2020-06-28 18:13:10 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/razrab', function () {
+    return view('razrab');
+});
 
 Route::get('/remont-kvartir', function () {
     return view('pages');
