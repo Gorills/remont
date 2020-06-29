d5510bb88d7164c2266a8653e357b91d6e0158f7
Gorills <gorivanickiy@gmail.com>
2020-06-29 12:48:58 +0700

    mail adress

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -29,7 +29,7 @@ class SendEmailController extends Controller
             'page'      => $request->type
         );
 
-        Mail::to('gorivanickiy@gmail.com')->send(new SendMail($data));
+        Mail::to('biggib@mail.ru')->send(new SendMail($data));
         return view('thank-you');
 
 
