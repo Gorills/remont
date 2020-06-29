99abc6c9c615d40695ec7d4b7275fd709cd0a2b9
Gorills <gorivanickiy@gmail.com>
2020-06-29 12:44:03 +0700

    thank

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -1,31 +1,17 @@
 @extends('layouts.custom')
 
 
-@section('title', 'Спасибо за заявку | Руд Медиум')
+@section('title', 'Спасибо за заявку | Прораб70')
 @section('description', 'Заявка принята и находится в обработке')
 @section('keywords', '')
 @section('canonical', 'https://beton70.com/thank-you')
-@section('og:title', 'Спасибо за заявку | Руд Медиум')
+@section('og:title', 'Спасибо за заявку | Прораб70')
 @section('og:description', 'Заявка принята и находится в обработке')
 @section('og:url', '')
 @section('twitter:description', 'Заявка принята и находится в обработке')
-@section('twitter:title', 'Спасибо за заявку | Руд Медиум')
+@section('twitter:title', 'Спасибо за заявку | Прораб70')
 
 @section('content')
-    <ul class="breadcrumb" itemscope="" itemtype="http://schema.org/BreadcrumbList">
-        <li class="breadcrumb-item" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
-            <a href="{{ URL('/') }}" itemprop="item">
-                <span itemprop="name">Главная</span>
-            </a>
-            <meta itemprop="position" content="1">
-        </li>
-        <li class="breadcrumb-item active breadcrumb-item--active" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
-            <a itemprop="item">
-                <span itemprop="name">Спасибо за заявку!</span>
-            </a>
-            <meta itemprop="position" content="2">
-        </li>
-    </ul>
 
 
     <div class="razrab">
