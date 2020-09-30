3f48e008fb8fe8fb145a27e5a85a50c57f419427
Gorills <gorivanickiy@gmail.com>
2020-09-30 13:31:52 +0700

    seo h3

--- resources/views/components/how.blade.php
+++ resources/views/components/how.blade.php
@@ -10,28 +10,28 @@
                 <div class="how__item">
 
                     <i class="how__icon far fas fa-ruler"></i>
-                    <h3 class="how__title">Делаем замеры</h3>
+                    <p class="how__title">Делаем замеры</p>
                     <p class="how__text">Наши специалисты оперативно просчитают объемы работ</p>
                     <div class="how__number">01</div>
                 </div>
                 <div class="how__item">
 
                     <i class="how__icon fas fa-pencil-alt"></i>
-                    <h3 class="how__title">Составляем смету</h3>
+                    <p class="how__title">Составляем смету</p>
                     <p class="how__text">Мы никогда не выходим за рамки согласованной сметы</p>
                     <div class="how__number">02</div>
                 </div>
                 <div class="how__item">
 
                     <i class="how__icon fas fa-file-contract"></i>
-                    <h3 class="how__title">Подписываем договор</h3>
+                    <p class="how__title">Подписываем договор</p>
                     <p class="how__text">В договоре мы прописываем все сроки и полную стоимость ремонта</p>
                     <div class="how__number">03</div>
                 </div>
                 <div class="how__item">
 
                     <i class="how__icon fas fa-hammer"></i>
-                    <h3 class="how__title">Приступаем к работе</h3>
+                    <p class="how__title">Приступаем к работе</p>
                     <p class="how__text">Все сроки и условия работы подкрепляются документами</p>
                     <div class="how__number">04</div>
                 </div>
